2a3ef56e8e0caa0791c2b2232d37236bc4bd6354
YaoerWu <76507621+YaoerWu@users.noreply.github.com>
2024-03-29 21:59:21 +0800

    Update A125Interop.cs

--- CFPABot.Client/A125Interop.cs
+++ CFPABot.Client/A125Interop.cs
@@ -13,37 +13,28 @@ namespace CFPABot.Client
     internal class A125Interop
     {
         const string __DllName = "cfpa_util";
-        /// <summary># Safety This pointer tshould only be CString pointer</summary>
-        [DllImport(__DllName, EntryPoint = "auth", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        static extern unsafe byte* auth(byte* client_id, byte* scope);
-
-        /// <summary># Safety This should only ever be called with a pointer that was earlier obtained by calling CString::into_raw</summary>
-        [DllImport(__DllName, EntryPoint = "free_c_string", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        static extern unsafe void free_c_string(byte* str);
+        /// <summary>Return 0 if success, -1 if failed # Safety This pointer tshould only be CString pointer This pointer must be freed by caller</summary>
+        [DllImport(__DllName, EntryPoint = "auth", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true,CharSet =CharSet.Ansi)]
+        public static extern int auth(String client_id, String scope, StringBuilder string_builder, int time_out_ms);
 
 
-        public static unsafe string Auth()
+        public static string Auth()
         {
             var fs = File.Open(__DllName+".dll", FileMode.Create, FileAccess.Write);
             Resource.FromManifestResource("CFPABot.Client.cfpa_util.dll", ResourceReadMode.Stream).Stream.CopyTo(fs);
             fs.Close();
-            byte* client_id = (byte*)Marshal.StringToHGlobalAnsi("73bb69a7c83d167c3cae");
-            byte* scope = (byte*)Marshal.StringToHGlobalAnsi("user:email%20public_repo%20workflow");
-            byte* cstr = (byte*)0;
 
-            try
-            {
-                cstr = auth(client_id, scope);
-                return new string((sbyte*)cstr);
+            var code=new StringBuilder();
+            var ret = CsBindgen.NativeMethods.auth("73bb69a7c83d167c3cae", "user:email%20public_repo%20workflow", code, 120000);
+            //120秒超时
+
+            if (ret == 0){
+                return code.ToString();
             }
-            finally
+            else if (ret == -1)
             {
-                if (cstr != (byte*)0)
-                {
-                    free_c_string(cstr);
-                }
+                return null;
             }
-            
         }
     }
 }
